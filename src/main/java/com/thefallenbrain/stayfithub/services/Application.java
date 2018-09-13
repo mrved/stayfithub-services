6353a1e0350008215f7f3a498026862bd7876625
Ved Sachdeva <ved.sachdeva@walmart.com>
2018-09-13 09:02:27 +0530

    update

--- src/main/java/com/thefallenbrain/stayfithub/services/Application.java
+++ src/main/java/com/thefallenbrain/stayfithub/services/Application.java
@@ -14,14 +14,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication(exclude = JmxAutoConfiguration.class)
-public class Application  {
+public class Application extends SpringBootServletInitializer {
 
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+        }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
